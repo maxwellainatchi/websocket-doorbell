7c9fbb18f0fc17ca61902da0bb4499bb52e73d78
Max Ainatchi <maxwell.ainatchi@gmail.com>
2018-06-28 15:40:18 +0200

    Updated styles. Enabled message on checkbox not fully working

---
+++ public/components/checkbox/checkbox.js
@@ -0,0 +1,18 @@
+var toggleCheckbox = function(el, callback) {
+    callback();
+    var verify = el.parentElement.lastElementChild;
+    verify.style.display = "initial";
+    verify.classList.add("disappearing");
+    verify.innerHTML = el.checked ? "Enabled!" : "Disabled!";
+    var listener = function(event) {
+        verify.style.display = "none";
+        verify.classList.remove("disappearing");
+        verify.removeEventListener("transitionend", listener, false);
+        verify.classList.add("disappearing--reset");
+        var innerListener = function(event) {
+            verify.classList.remove("disappearing--reset");
+        }
+        verify.addEventListener("transitionend", innerListener, false)
+    };
+    verify.addEventListener("transitionend", listener, false)
+}
